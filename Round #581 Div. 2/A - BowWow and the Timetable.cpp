4311138d4213cbdf723bc88cd885e7c625e06aad
lojpark <lojpark@gmail.com>
2019-08-21 18:56:33 +0900

    Math

---
+++ Round #581 Div. 2/A - BowWow and the Timetable.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string a;
+	int one = 0;
+	cin >> a;
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i] == '1') {
+			one++;
+		}
+	}
+
+	if (a.size() == 1 && a[0] == '0') {
+		cout << "0";
+		return 0;
+	}
+
+	if (a.size() % 2 == 1) {
+		if (one == 1) {
+			cout << (a.size() >> 1);
+		}
+		else {
+			cout << (a.size() >> 1) + 1;
+		}
+	}
+	else {
+		cout << (a.size() >> 1);
+	}
+
+	return 0;
+}
